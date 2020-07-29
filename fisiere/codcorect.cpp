44c316e2707aecbfd67df2516fc5d86e874e7971
VanessaCupsan <67056723+VanessaCupsan@users.noreply.github.com>
2020-07-29 11:51:17 +0300

    Adaugare fisiere necesare executabilului.

---
+++ fisiere/codcorect.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+ifstream fin("test.txt");
+int n, a[100][100], i, j, s,m;
+int main()
+        {
+            fin >> n>>m;
+            for (i = 1; i <= n; i++)
+                for (j = 1; j <= n; j++)
+                    fin >> a[i][j];
+            for(i = 1; i<=n; i++)
+                for(j= i+1; j<= n; j++)
+                cout <<a[i][j]<<" ";
+            return 0;
+        }
+            
---
+++ fisiere/codelev.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+ifstream fin("test.txt");
+int n, m, a[100][100], i, j, s;
+int main()
+        {
+            fin >> n >> m;
+            for (i = 1; i <= n; i++)
+                for (j = 1; j <= m; j++)
+                    fin >> a[i][j]; 
+for(i=1;i<=n;i++)
+for(j=i+1;j<=n;j++)
+cout<<a[i][j]<<" "
+return 0; }
